3f9ff8f8c56a3f5d2008860b5f6e2510bc9a2d08
jao0609 <batistadepaivajoaovitor250609@gmail.com>
2026-03-13 14:07:54 -0300

    Implementação do arquivo application.properties e execução do projeto

--- ProjetoEtecfer-main/src/main/java/br/com/etecfer/etecfer/entity/Aluno.java
+++ ProjetoEtecfer-main/src/main/java/br/com/etecfer/etecfer/entity/Aluno.java
@@ -5,23 +5,36 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
  
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 public class Aluno {
- 
+    
+    //DEfinição dos atributos da entidade Aluno
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer idAluno;
     
     @Column(nullable = false,length = 40)
     private String nomeAluno;
+
     @Column(length = 40)
     private String emailAluno;
+
     @Column(nullable = false,length = 11)
     private String telefoneAluno;
+
     @Column(nullable = false)
     private Integer raAluno;
+
     @Column(nullable = false,length = 11)
     private String cpfAluno;
-   
+      
 }
